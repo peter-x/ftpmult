11aff017c6cc0a0c685c83204f733b66364c36b9
cycloon <cycloon@is-root.org>
2014-01-28 23:43:42 +0100

    Revert "make EPSV wiring"
    This reverts commit 59004405aab7e5248ee12638ddf9359d5cc5e993.

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 var ftpd = require('./ftpmult.js');
 
 try {
-    process.setuid(0);
-    process.setgid(0);
+    process.setuid(65534);
+    process.setgid(65534);
 } catch (e) {
 }
 
 var server = new ftpd.FtpServer({
     getHostFromUsername: function(username) {
-        return {hostname: 'protexneu.cnc-nt.de', port: 21};
+        return {hostname: 'ftp.kernel.org', port: 21};
     }
 });
 
-server.logLevel = 5;
-server.listen(21);
+server.logLevel = 4;
+server.listen(7002);
 server.server.on("error", function() {
     /* better exit so that someone can restart us */
     process.exit(1);
